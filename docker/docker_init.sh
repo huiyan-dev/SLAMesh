a8663e82dac8113a230b85d248fa03c39ebe7eac
liuhuiyan9 <liuhuiyan9@jd.com>
2023-09-01 18:40:01 +0800

    docker : add init shell

---
+++ docker/docker_init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+WORK_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+echo "current directory is $WORK_PATH"
+
+docker pull pleaserun/rjy_slam_work:slamesh_18.04
+
+docker run -it -p 5900:5900 -p 2222:22 -e RESOLUTION=1920x1080  \
+            -v "$WORK_PATH"/datasets/:/root/dataset \
+            -v "$HOME"/huiyan_ws/opensource/:/root/workspace/ \
+            --name test_slamesh \
+            pleaserun/rjy_slam_work:slamesh_18.04

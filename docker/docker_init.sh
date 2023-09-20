613731ee132975727ac3c5432cefb0591656f212
huiyan-dev <lhy.fullstack@gmail.com>
2023-09-20 10:28:56 +0000

    add docker shell

--- docker/docker_init.sh
+++ docker/docker_init.sh
@@ -2,12 +2,12 @@
 
 WORK_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-echo "current directory is $WORK_PATH"
+echo "current directory is {$WORK_PATH}"
 
 docker pull pleaserun/rjy_slam_work:slamesh_18.04
 
-docker run -it -p 5900:5900 -p 2222:22 -e RESOLUTION=1920x1080  \
-            -v "$WORK_PATH"/datasets/:/root/dataset \
+docker run -it -d -p 5900:5900 -p 2222:22 -e RESOLUTION=1920x1080  \
+            -v "$WORK_PATH"/datasets/:/root/datasets \
             -v "$HOME"/huiyan_ws/opensource/:/root/workspace/ \
             --name test_slamesh \
             pleaserun/rjy_slam_work:slamesh_18.04
